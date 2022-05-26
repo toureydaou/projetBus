d0684bca117e1310684ed65303ea6453e10f8053
samTeouri <teourisamrou@gmail.com>
2022-05-26 13:23:32 +0000

    migration des tables et création des modèles

---
+++ fullTransport/app/Models/Client.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Client extends Model
+{
+    use HasFactory;
+    use softDeletes;
+
+    protected $fillable = ['nbPlaces', 'marque', 'climatisation', 'statut'];
+    protected $primaryKey = 'idClient';
+
+    public function colis() {
+        return $this->hasMany('app/Models/Colis', 'idColis');
+    }
+
+    public function ticket() {
+        return $this->hasMany('app/Models/Ticket', 'idTicket');
+    }
+
+}
---
+++ fullTransport/app/Models/Ligne.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Ligne extends Model
+{
+    use HasFactory;
+    use softDeletes;
+
+    protected $fillable = ['prix', 'depart', 'arrivee'];
+    protected $primaryKey = 'idLigne';
+
+    public function voyages() {
+        return $this->hasMany('app/Models/Voyage', 'idVoyage');
+    }
+}
