ebd4305ca440bf405c1db3ed089218fe642f5356
samTeouri <teourisamrou@gmail.com>
2022-06-05 15:44:42 +0000

    codage des fonctions des controlleurs et création des routes pour les modèle Bus.

---
+++ fullTransport/app/Http/Controllers/BusController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bus;
+use Illuminate\Http\Request;
+
+class BusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $buses = Bus::all();
+        return view('bus.index')->with('listeBus', $buses);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('bus.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $bus = Bus::create(['nbPlaces' => $request["nbPlaces"],
+            'marque' => $request["marque"],
+            'climatisation' => $request["climatisation"],
+            'statut' => $request["statut"]]);
+        $bus->save();
+        return redirect()->route('listeBus');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Bus  $bus
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Bus $bus)
+    {
+        return view('bus.show')->with('bus', $bus);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Bus  $bus
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Bus $bus)
+    {
+        return view('bus.edit',)->with('bus', $bus);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Bus  $bus
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Bus $bus)
+    {
+        $bus->nbPlaces = $request["nbPlaces"];
+        $bus->marque = $request["marque"];
+        $bus->climatisation = $request["climatisation"];
+        $bus->statut = $request["statut"];
+        $bus->save();
+        return redirect()->route('listeBus');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Bus  $bus
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Bus $bus)
+    {
+        $bus->delete();
+        return redirect()->route('listeBus');
+    }
+}
+
---
+++ fullTransport/database/migrations/2022_05_26_143027_create_bus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatebusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bus', function (Blueprint $table) {
+            $table->bigIncrements('idBus');
+            $table->integer('nbPlaces');
+            $table->string('marque');
+            $table->boolean('climatisation');
+            $table->string('statut');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bus');
+    }
+}
